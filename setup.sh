8d6a195d5bb8e667c6f000ec8877181a80ef0c1b
zer0tim3r <zer0tim3r@gmail.com>
2024-10-14 17:39:05 +0900

    new file:   direct.xml 	modified:   helio-cloudinit/Cargo.toml 	modified:   helio-dhcp/Cargo.toml 	modified:   helio-pg/migrations/2024-10-11-120327_create_tables/up.sql 	modified:   setup.sh

--- setup.sh
+++ setup.sh
@@ -4,6 +4,7 @@ sudo firewall-cmd --permanent --zone=public --add-forward
 
 sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 2 -j DROP
 sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 1 ! -s 192.168.10.0/24 -j ACCEPT
+
 sudo firewall-cmd --permanent --direct --add-rule ipv4 filter FORWARD 0 -s 192.168.10.3 -m mac --mac-source 52:54:00:11:22:33 -j ACCEPT
 
 sudo firewall-cmd --permanent --direct --add-rule ipv4 nat PREROUTING 0 -d 169.254.169.254 -p tcp --dport 80 -j DNAT --to-destination 192.168.10.254:8180
